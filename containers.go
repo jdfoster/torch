ba0e77385e548cc6af6eaee87478f33638179cdf
jdfoster <joshua.david.foster@gmail.com>
2022-04-15 11:14:00 +0100

    feat: build pipeline to dynamically scrap wiki data

---
+++ containers.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	tc "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+type redis struct {
+	tc.Container
+	Hostname, Port, URI string
+}
+
+func setupRedis(ctx context.Context) (*redis, error) {
+	req := tc.ContainerRequest{
+		Image:        "redis",
+		ExposedPorts: []string{"6379/tcp"},
+		WaitingFor:   wait.ForListeningPort("6379").WithStartupTimeout(10 * time.Second),
+	}
+
+	c, err := tc.GenericContainer(ctx, tc.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("setupRedis, failed to start container: %w", err)
+	}
+
+	h, err := c.Host(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("setupRedis, failed to extract container IP: %w", err)
+	}
+
+	// fallback to default docker bridge gateway
+	if h == "localhost" {
+		h = "172.17.0.1"
+	}
+
+	p, err := c.MappedPort(ctx, "6379/tcp")
+	if err != nil {
+		return nil, fmt.Errorf("setupRedis, failed to extract container port: %w", err)
+	}
+
+	r := &redis{
+		Container: c,
+		Hostname:  h,
+		Port:      p.Port(),
+		URI:       fmt.Sprintf("redis://%s:%s", h, p.Port()),
+	}
+
+	return r, nil
+}
+
+type benthos struct {
+	tc.Container
+	Hostname, Port, URI string
+}
+
+func setupBenthos(ctx context.Context, confPath string, env map[string]string) (*benthos, error) {
+	req := tc.ContainerRequest{
+		Image:        "jeffail/benthos",
+		ExposedPorts: []string{"4195/tcp"},
+		Mounts: tc.ContainerMounts{
+			{
+				Source:   tc.GenericBindMountSource{HostPath: confPath},
+				Target:   "/opt/benthos",
+				ReadOnly: true,
+			},
+		},
+		Env:        env,
+		WaitingFor: wait.ForHTTP("/ready").WithPort("4195").WithStartupTimeout(10 * time.Second),
+		Cmd:        []string{"-c", "/opt/benthos/benthos.yaml"},
+	}
+
+	c, err := tc.GenericContainer(ctx, tc.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("setupBenthos, failed to start container: %w", err)
+	}
+
+	h, err := c.Host(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("setupBenthos, failed to extract container IP: %w", err)
+	}
+	// fallback to default docker bridge gateway
+	if h == "localhost" {
+		h = "172.17.0.1"
+	}
+
+	p, err := c.MappedPort(ctx, "4195/tcp")
+	if err != nil {
+		return nil, fmt.Errorf("setupBenthos, failed to extract container port: %w", err)
+	}
+
+	b := &benthos{
+		Container: c,
+		Hostname:  h,
+		Port:      p.Port(),
+		URI:       fmt.Sprintf("http://%s:%s", h, p.Port()),
+	}
+
+	return b, nil
+}
--- main.go
+++ main.go
@@ -1,6 +1,71 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"path"
+	"time"
+)
+
+type terminator interface {
+	Terminate(context.Context) error
+}
+
+func setup(ctx context.Context) (stop func(context.Context), err error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Minute*4)
+	defer cancel()
+
+	var t []terminator
+	stop = func(ctx context.Context) {
+		for _, c := range t {
+			c.Terminate(ctx)
+		}
+	}
+
+	defer func() {
+		// terminate any created containers on error
+		if err != nil {
+			stop(ctx)
+			t = []terminator{}
+			err = fmt.Errorf("failed to setup containers: %w", err)
+		}
+	}()
+
+	r, err := setupRedis(ctx)
+	if err != nil {
+		return
+	}
+	t = append(t, r.Container)
+
+	wd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	dir := path.Join(wd, "pipeline")
+
+	b, err := setupBenthos(ctx, path.Join(dir, "dynamic_ingest"), map[string]string{"REDIS_URI": r.URI})
+	if err != nil {
+		return
+	}
+
+	_, err = setupBenthos(ctx, path.Join(dir, "wiki_scraper"), map[string]string{"REDIS_URI": r.URI, "DYNAMIC_INGEST_URL": b.URI })
+
+	return
+}
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
+	cancel, err := setup(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	<-ctx.Done()
+	fmt.Println("shutting down")
+	cancel(context.Background())
 }
